f88089d2e6e9185443fc8e3686ded8acc6ceecef
jay800427 <jay8000427@gmail.com>
2019-11-18 13:20:01 +0800

    刪除使用者與前端的操作

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -1,5 +1,11 @@
 <?php
 //處理刪除資料的請求
+include_once "base.php";
 
+$id=$_POST['id'];
+$sql="delete from students where uni_id='$id'";
+
+echo $pdo->exec($sql);//query回傳的是一個陣列,exec是檔案有無上傳成功，回傳的值是0或1
+echo "你刪除的是學號".$id."的學生";
 
 ?>
--- app2/api/query.php
+++ app2/api/query.php
@@ -1,7 +1,7 @@
 <?php
 //處理查詢資料的請求
-$dsn="mysql:host=localhost;charset=utf8;dbname=ajax";
-$pdo=new PDO($dsn,"root","");
+include_once "base.php";
+
 $class='1'. sprintf("%02d",$_GET["class"]);
 /* echo $class;
 echo "<br>"; */
@@ -14,6 +14,7 @@ echo "<td>姓名</td>";
 echo "<td>科系</td>";
 echo "<td>生日</td>";
 echo "<td>學號</td>";
+echo "<td>操作</td>";
 echo "</tr>";
 foreach($rows as $r){
     echo "<tr>";
@@ -22,6 +23,10 @@ foreach($rows as $r){
     echo "<td>".$r['dept']."</td>";
     echo "<td>".$r['birthday']."</td>";
     echo "<td>".$r['uni_id']."</td>";
+    echo "<td>";
+    echo "<button class='edit-user' data-edit='".$r['uni_id']."'>編輯</button>";
+    echo "<button class='del-user' data-del='".$r['uni_id']."'>刪除</button>";
+    echo "</td>";
     echo "</tr>";
 }
 ?>
